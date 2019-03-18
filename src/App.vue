c6906367d7a48c4d0fe8174a7cab013fd0e7f3d8
tasim <t.simon@s-v.de>
2019-03-18 14:16:12 +0100

    Add Navigation and footer

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <Header></Header>
+      <HelloWorld msg="Welcome to Track Your Needs!"/>
+
+      <footer class="footer">
+        <p>© 2019 Tabea Simon</p>
+      </footer>
+    </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Header
   }
 }
 </script>
 
 <style>
+body, html {
+  margin: 0px !important;
+}
 #app {
+  height: 100vh;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.container {
+  position: relative;
+  height: 100%;
+}
+
+footer {
+  position: absolute;
+  height: 40px;
+  width: 100%;
+  bottom: 0;
+  background-color: #C4C4C4;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="nav navbar">
+        <h1 class="app_name">Track your Needs</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+.nav {
+    background-color: #C4C4C4;
+    text-align: left;
+    height: 40px
+}
+
+.app_name {
+    font-size: 15px;
+    padding: 5px;
+    margin: 0px
+}
+
+</style>
